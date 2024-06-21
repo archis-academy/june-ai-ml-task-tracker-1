e7dd4f2b66949af3df4c10979a159132c1f7bece
Taha <tahagill99@gmail.com>
2024-06-21 15:36:45 +0300

    [JU-3]   -moved code to proper class

--- src/task.py
+++ src/task.py
@@ -13,3 +13,15 @@ class Task:
         self.subtasks = []
         self.comments = []
         self.tags = []
+
+    def get_info(self):
+        info = (
+            f"Ticket number: {self.ticket_number}\n"
+            f"Id: {self.id}\n"
+            f"Title: {self.title}\n"
+            f"Description: {self.description}\n"
+            f"Duration: {self.duration}\n"
+            f"Priority: {self.priority}\n"
+            f"Status: {self.status}\n"
+            f"Creation Date: {self.creation_date}\n")
+        return info
--- src/task_manager.py
+++ src/task_manager.py
@@ -2,3 +2,26 @@ class TaskManager:
     def __init__(self):
         self.tasks = {}  # key value
         self.next_id = 1
+    def sort_by_creation_date(self):
+        self.tasks.sort(key=lambda task: task.creation_date)
+
+    def sort_by_status(self):
+        pending_tasks = []
+        completed_tasks = []
+        cancelled_tasks = []
+
+        for task in self.tasks:
+            if task.status == 'pending':
+                pending_tasks.append(task)
+            elif task.status == 'completed':
+                completed_tasks.append(task)
+            elif task.status == 'cancelled':
+                cancelled_tasks.append(task)
+            else:
+                print('Invalid status:', task.status)
+
+        return pending_tasks, completed_tasks, cancelled_tasks
+
+    def get_all_info(self):
+        for task in self.tasks:
+            print(task.get_info())
