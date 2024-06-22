795b7df2d910a6ef5be6b67c81bad630b2a1920b
FDenizYuksekbulgu <145774283+FDenizYuksekbulgu@users.noreply.github.com>
2024-06-22 17:39:59 +0300

    Update task_manager.py

--- src/task_manager.py
+++ src/task_manager.py
@@ -1,4 +1,12 @@
-class TaskManager:
+class UserManager:
     def __init__(self):
-        self.tasks = {}  # key value
-        self.next_id = 1
+        self.users = {} #we are going to use dictionary data type in order to store user object with a key value pair
+
+        #for example,{"example@gmail.com" : user object}
+
+
+    def authenticate_user(self,username,password):
+        user = self.users.get(username)
+        if user and user.password == password:
+            return user
+        return None
