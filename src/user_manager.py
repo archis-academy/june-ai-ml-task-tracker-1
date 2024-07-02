4ab8d7a4a772ae754f6d9599fe18709645171cac
Taha <tahagill99@gmail.com>
2024-07-02 14:32:55 +0300

    [JU-5]   -added register new user feature

--- src/user_manager.py
+++ src/user_manager.py
@@ -1,9 +1,21 @@
+from user import User
 class UserManager:
     def __init__(self):
         self.users = {}  # key value {username:user_object}
+        self.next_id = 1
 
     def authenticate_user(self, username, password):
         user = self.users.get(username)
         if user and user.password == password:
             return user
         return None
+    
+    def register_user(self, username, password):
+        if username in self.users:
+            print("Username already exists.")
+            return None
+        user = User(self.next_id, username, password)
+        self.users[username] = user
+        self.next_id += 1
+        print("User registered successfully.")
+        return user
