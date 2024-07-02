74f5998c79e48dca9ea086b33d802f23e380dd3f
Taha <tahagill99@gmail.com>
2024-07-02 15:08:20 +0300

    [JU-18] added user logout feature updated CLI

--- src/cli.py
+++ src/cli.py
@@ -3,3 +3,31 @@ class CLI:
         self.task_manager = task_manager
         self.user_manager = user_manager
         self.current_user = None
+## my run method from previous commits of prev branches updated to handle logout as well when executed. new branch thats why copied form previous
+    def run(self):
+        while True:
+            command = input("Enter command (register/login/logout/add_task/display_tasks/update_task/exit): ").strip().lower() 
+            if command == "register":
+                self.register()
+            elif command == "login":
+                self.login()
+            elif command == "logout":
+                self.logout() 
+            elif command == "add_task":
+                self.add_task()
+            elif command == "display_tasks":
+                self.display_tasks()
+            elif command == "update_task":
+                self.update_task()
+            elif command == "exit":
+                break
+            else:
+                print("Invalid command.")
+
+    def logout(self):
+
+        if self.current_user:
+            self.current_user = None
+            print("Logged out successfully.")  
+        else:
+            print("No user is currently logged in.")
